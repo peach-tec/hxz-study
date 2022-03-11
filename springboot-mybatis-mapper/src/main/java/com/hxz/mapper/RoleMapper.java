b74b7c31c8334479308f4708782b3bc0c268ab0e
丁银华 <18482133793@163.com>
2022-03-11 15:52:36 +0800

    代码生成器修改配置进行测试

--- springboot-mybatis-mapper/src/main/java/com/hxz/mapper/RoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hxz.mapper;
-
-import com.hxz.entity.Role;
-import org.springframework.stereotype.Repository;
-import tk.mybatis.mapper.common.Mapper;
-
-@Repository
-public interface RoleMapper extends Mapper<Role> {
-}
