8db0a76058c113e6c2f5b5f9b4933b3488acf086
华小灼 <18482133793@163.com>
2022-04-23 22:33:30 +0800

    eureka注册中心

---
+++ eureka-server-demo01/src/main/java/com/hxz/eureka/EurekaServerDemo01Application.java
@@ -0,0 +1,13 @@
+package com.hxz.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // 开启Eureka主中心功能并启动
+public class EurekaServerDemo01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerDemo01Application.class, args);
+    }
+}
