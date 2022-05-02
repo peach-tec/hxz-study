8f965867f8de30b548e5f0f8402d7e75304fc804
华小灼 <18482133793@163.com>
2022-05-02 23:58:43 +0800

    网关调优，超时时长

--- zuul-server-demo/src/main/java/com/hxz/zuul/ZuulServerDemoApplication.java
+++ zuul-server-demo/src/main/java/com/hxz/zuul/ZuulServerDemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableZuulProxy // 开启Zuul代理
 @EnableHystrixDashboard // 开启数据监控
+@EnableRetry // 开启重试机制
 public class ZuulServerDemoApplication {
 
     public static void main(String[] args) {
